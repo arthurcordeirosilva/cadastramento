c9fae0140e9ca78201a48f1620500dc8004e0d7c
David Pascoal Silva <ednaldo.chance.orkut@gmail.com>
2024-10-01 22:29:21 -0300

    Adicionando telas e assets do projeto

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Welcome from '../screens/Welcome';
+import SingIn from '../screens/SingIn';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{  headerShown: false}}
+            />
+               <Stack.Screen
+                 name="SingIn"
+                 component={SingIn}
+                 options={{  headerShown: false}}
+            />
+        </Stack.Navigator>
+    )
+}
